a1b1561a7639c4260fb0376e6a10ab4c2f15f46c
Alexander Panasyuk <panasmeister@gmail.com>
2013-11-29 13:00:18 +0800

    Liza fixes

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,14 @@
+require 'uri'
+
 module GroupsHelper
+
+  def vk_post text=''
+    uris = URI.extract text, ['http', 'https']
+    result = text.dup
+    uris.each do |uri|
+      result.gsub! uri, "<a href=\"#{uri}\" target=\"_blank\">#{uri}</a>"
+    end
+    raw result
+  end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -55,21 +55,26 @@ class Group < ActiveRecord::Base
         'posts.group_id',
         'groups.id'
     ).order('posts.published_at DESC').references(:posts)
+    wrap_text_style = xls.workbook.styles.add_style alignment: { wrap_text: true }, height: 'auto'
     relation.each do |group|
       sheet = xls.workbook.add_worksheet name: group.screen_name
-      sheet.add_row ['Дата', 'Текст', 'Кол-во лайков', 'Кол-во комментариев', 'Кол-во репостов', 'URL']
+      sheet.add_row ['Дата', 'Текст', 'URL', 'Кол-во лайков', 'Кол-во комментариев', 'Кол-во репостов']
       posts_count = 1
       group.posts.each do |post|
         posts_count+=1
-        sheet.add_row [post.published_at, post.text.gsub(/<\/?[^>]+?>/, ''), post.likes_count, post.comments_count, post.reposts_count, "#{group.url}?w=wall-#{group.gid}_#{post.vk_id}"], style: [time_style, nil, nil, nil, nil, nil]
+        post_text = post.text.gsub(/<\/?[^>]+?>/, '')
+        row = sheet.add_row [post.published_at, post_text, "#{group.url}?w=wall-#{group.gid}_#{post.vk_id}", post.likes_count, post.comments_count, post.reposts_count], style: [time_style, wrap_text_style, nil, nil, nil, nil]
+        text_column_width = 408
+        row_height = (post_text.length*8+5)/text_column_width*20
+        row.height = row_height<23 ? 30 : row_height
       end
 
-      sheet.add_row [nil, nil, "=SUM(C2:C#{posts_count})", "=SUM(D2:D#{posts_count})", "=SUM(E2:E#{posts_count})"]
+      sheet.add_row [nil, nil, "=SUM(D2:D#{posts_count})", "=SUM(E2:E#{posts_count})", "=SUM(F2:F#{posts_count})"]
       sheet.column_info[0].width=17
       sheet.column_info[1].width=50
-      sheet.column_info[2].width=12
-      sheet.column_info[3].width=18
-      sheet.column_info[4].width=13
+      sheet.column_info[3].width=12
+      sheet.column_info[4].width=18
+      sheet.column_info[5].width=13
     end
     xls
   end
